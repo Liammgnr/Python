96bc342940bbb7574b08439efd179c34ef8bc654
Liammgnr <167356611+Liammgnr@users.noreply.github.com>
2024-10-30 01:03:19 +0100

    Delete input_shop.py

--- input_shop.py
+++
@@ -1,8 +0,0 @@
-# Exercice 2 Shopping Cart Program
-
-item = input("Enter the item you want to buy :")
-price = float(input("Enter the price of the item :"))
-quantity = int(input("Enter the quantity of the item :"))
-total = price * quantity
-
-print(f"Your {quantity} {item}(s) will cost you {total} euros.")
